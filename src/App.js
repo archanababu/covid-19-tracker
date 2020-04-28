9235be694640635961973496f508bf0dbb86ab63
arcbabu <archana.masterseducation@gmail.com>
2020-04-28 17:39:43 -0500

    Adding image and media styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import coronaImage from './img/image.png'
 
 class App extends React.Component{
     state={
@@ -26,6 +27,7 @@ class App extends React.Component{
         const {data, country} = this.state;
         return(
             <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt="COVID-19"/>
                 <Cards data = {data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart data = {data} country = {country} />
