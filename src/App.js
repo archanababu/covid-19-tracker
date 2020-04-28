358b7d54e71702908a72916aae3f6c43bda840c7
arcbabu <archana.masterseducation@gmail.com>
2020-04-28 17:03:17 -0500

    Implementing barChart for countryPicker module

--- src/App.js
+++ src/App.js
@@ -15,19 +15,20 @@ class App extends React.Component{
     }
 
     handleCountryChange = async(country) => {
-        const fetchedData = await fetchData(country);
-        console.log(fetchedData);
-        
+        //fetch the data
+        const fetchedData = await fetchData(country);               
         //set the state
+        this.setState({data: fetchedData, country: country});
+
     }
     
     render(){
-        const {data} = this.state;
+        const {data, country} = this.state;
         return(
             <div className={styles.container}>
                 <Cards data = {data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
-                <Chart />
+                <Chart data = {data} country = {country} />
             </div>
         )
     }
